7e9801c5184e0e8e1659bd101d695eb54c16d590
Karts <9369992@gmail.com>
2017-10-29 20:45:10 +0300

    New background and nav-bar in page

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,9 @@
 require("../css/normalize.css");
 require("../css/style.scss");
+
+
+$(".nav-item").click(function() {
+
+    $("li.nav-item.active").removeClass("active");
+    $(this).addClass('active');
+});
