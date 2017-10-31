940caf3acc9718d24a4b47d48cacf08629c4ae27
Karts <9369992@gmail.com>
2017-11-01 02:13:49 +0300

    New responsev image ground

--- src/js/index.js
+++ src/js/index.js
@@ -13,3 +13,11 @@ $(".sun").click(function() {
 
     console.log("check");
 });
+
+window.onscroll = function() {scrolling()};
+
+function scrolling() {
+    let stepGrayScale =100 / ($(document).height() - $(window).height());
+    let stepContrast = 50 / ($(document).height() - $(window).height());
+    document.getElementById("img-ground").style.webkitFilter = "grayscale("+ (100 - stepGrayScale * $(window).scrollTop()) +"%)  contrast(" + (150 - stepContrast * $(window).scrollTop()) +  "%)";
+}
